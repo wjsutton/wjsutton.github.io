cd7ecdcb1c8cf7a1aeae81ed1cdfedc0d7468787
Will Sutton <sutton_999@hotmail.com>
2020-05-24 16:43:35 +0100

    clean up

--- _site/assets/d3/makeover_monday_2020w14/mom_2020w14_legend.js
+++ _site/assets/d3/makeover_monday_2020w14/mom_2020w14_legend.js
@@ -17,7 +17,7 @@ const legend = d3.select("#legend")
 // Adding legend manually
 legend.append("rect").attr("height",20).attr("width",20).attr("x",270 /2).attr("y",0).style("fill", "#445E93")
 legend.append("rect").attr("height",20).attr("width",20).attr("x",370 /2).attr("y",0).style("fill", "#7EB2DD")
-legend.append("text").attr("x", 162 /2).attr("y", 12).text("Paid work").style("font-size", "13px").attr("alignment-baseline","middle").attr("fill", "black")
+legend.append("text").attr("x", 160 /2).attr("y", 12).text("Paid work").style("font-size", "13px").attr("alignment-baseline","middle").attr("fill", "black")
 legend.append("text").attr("x", 412 /2).attr("y", 12).text("Unpaid work").style("font-size", "13px").attr("alignment-baseline","middle").attr("fill", "black")
 
 // Adding text at either end of div for author and source
